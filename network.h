f930db67e9e7d4ed8dee14da3c717b3317a4b693
rangbou <abou@usc.edu>
2020-04-26 23:08:48 -0700

    netwrok.h

--- network.h
+++ network.h
@@ -30,6 +30,8 @@ class Network{
         void printDB();
         void showMenu();
         void return_search(string fname, string lname, string bdate);
+        //phase 2
+        void friendRecommend(int k)
 };
 
 #endif
